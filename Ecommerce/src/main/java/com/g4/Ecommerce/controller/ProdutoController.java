7446adbd914da757ebf3507c70f264289b9f715f
renatons17 <83104807+renatons17@users.noreply.github.com>
2021-06-03 14:31:21 -0300

    ProdutoRepository + Correções nos Controllers + Gets/Setters da model Produto

--- Ecommerce/src/main/java/com/g4/Ecommerce/controller/ProdutoController.java
+++ Ecommerce/src/main/java/com/g4/Ecommerce/controller/ProdutoController.java
@@ -1,64 +1,72 @@
 package com.g4.Ecommerce.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.g4.Ecommerce.model.Produto;
+import com.g4.Ecommerce.repository.ProdutoRepository;
+
 @RestController
-@RequestMapping ("/produto")
-// @CrossOrigin
+@RequestMapping("/produto")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 
 public class ProdutoController {
 
-	
 	@Autowired
-	private ProdutoRepository repository; 
-	
-	//FindAll
+	private ProdutoRepository repository;
+
+	// FindAll
 	@GetMapping
-	public ResponseEntity<List<Produto>> FindAll () {
-		return ResponseEntity.ok(repository.findAll ());
+	public ResponseEntity<List<Produto>> FindAll() {
+		return ResponseEntity.ok(repository.findAll());
 	}
-	
-	//FindById
-	@GetMapping ("/{id}")
-	public ResponseEntity<Produto> FindById (@PathVariable long id) {
-		return repository.findById(id).map(resposta -> ResponseEntity.ok(resposta)).orElse(ResponseEntity.notFound().build());
+
+	// FindById
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> FindById(@PathVariable long id) {
+		return repository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
 	}
-	
-	//MetodoEspecifico
-	
-		
-	//Post
+
+	// MetodoEspecifico
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produto>> FindByName(@PathVariable String nome) {
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Produto>> FindByDescricao(@PathVariable String descricao) {
+		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
+	}
+
+	// Post
 	@PostMapping
-	public ResponseEntity<Produto> post (@RequestBody Produto produto) {
+	public ResponseEntity<Produto> post(@RequestBody Produto produto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
 	}
-			
-	//Put
+
+	// Put
 	@PutMapping
-	public ResponseEntity<Produto> put (@RequestBody Produto produto) {
+	public ResponseEntity<Produto> put(@RequestBody Produto produto) {
 		return ResponseEntity.ok(repository.save(produto));
 	}
-			
-	//Delete
-	@DeleteMapping ("/{id}")
-	public void delete (@PathVariable long id) {
+
+	// Delete
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
 }
---
+++ Ecommerce/src/main/java/com/g4/Ecommerce/repository/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package com.g4.Ecommerce.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.g4.Ecommerce.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+
+	public List<Produto> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
