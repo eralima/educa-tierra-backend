74780087155a1d831ae0d65efe8bd3efba8a6056
israel-97 <israel.dantas1@gmail.com>
2021-06-10 19:59:49 -0300

    Atualizacao

--- Ecommerce/src/main/java/com/g4/Ecommerce/security/BasicSecurityConfig.java
+++ Ecommerce/src/main/java/com/g4/Ecommerce/security/BasicSecurityConfig.java
@@ -1,14 +1,17 @@
 package com.g4.Ecommerce.security;
 
-import javax.servlet.annotation.HttpMethodConstraint;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,10 +20,25 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
+    @Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+		
 	@Override
 	protected void configure(HttpSecurity Http) throws Exception {
 		Http.authorizeRequests().antMatchers(HttpMethod.POST, "/cadastro").permitAll()
-		.antMatchers(HttpMethod., null) //oi
+		.antMatchers(HttpMethod.POST, "/login").permitAll() 
+		.anyRequest().authenticated()
+		.and().httpBasic()
+		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().cors()
+		.and().csrf().disable();
 	}
 	
 }
