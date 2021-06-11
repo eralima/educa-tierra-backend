53f801240f8e93b70e9ccde2f8aca2aabd805d9d
renatons17 <83104807+renatons17@users.noreply.github.com>
2021-06-10 22:12:28 -0300

    Controller do Usuario

--- Ecommerce/src/main/java/com/g4/Ecommerce/controller/UsuarioController.java
+++ Ecommerce/src/main/java/com/g4/Ecommerce/controller/UsuarioController.java
@@ -1,7 +1,24 @@
 package com.g4.Ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/usuario")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 	
+//	@Autowired
+//	private UsuarioService usuarioService;
 	
+	@PostMapping("/login")
+	public ResponseEntity<UserLogin> auth (@RequestBody Optional<UserLogin> user){
+		return usuarioService.
+				
+				
+
+	}	
 
 }
